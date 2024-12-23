22ac9df15821e39bbffec96ea2bba5f3316c2422
Vyacheslav <guzxaroh@gmail.com>
2024-12-24 02:26:04 +0700

    implemented service "stay user log in if he exit from app"

---
+++ Eltex_Project/Eltex_Project/AppCoordinator/UserCacheService/UserCacheService.swift
@@ -0,0 +1,64 @@
+//
+//  UserCacheService.swift
+//  Eltex_Project
+//
+//  Created by Vyacheslav Gusev on 23.12.2024.
+//
+
+import Foundation
+
+final class UserCacheService {
+    static let shared = UserCacheService()
+    
+    private let cache = NSCache<NSString, UserInfoWrapper>()
+    private let fileManager = FileManager.default
+    private let fileName = "authenticatedUser.json"
+    
+    private lazy var filePath: URL = {
+        fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(fileName)
+    }()
+    
+    private init() {
+        loadUserFromDisk()
+    }
+    
+    func saveUser(_ user: UserInfo) {
+        let wrapper = UserInfoWrapper(userInfo: user)
+        cache.setObject(wrapper, forKey: "authenticatedUser")
+        saveUserToDisk(user)
+    }
+    
+    func getUser() -> UserInfo? {
+        if let wrapper = cache.object(forKey: "authenticatedUser") {
+            return wrapper.userInfo
+        }
+        return nil
+    }
+    
+    func clearUser() {
+        cache.removeObject(forKey: "authenticatedUser")
+        try? fileManager.removeItem(at: filePath)
+    }
+    
+    private func saveUserToDisk(_ user: UserInfo) {
+        do {
+            let data = try JSONEncoder().encode(user)
+            try data.write(to: filePath, options: .atomic)
+        } catch {
+            fatalError("Failed to save user to disk: \(error)")
+        }
+    }
+    
+    private func loadUserFromDisk() {
+        guard fileManager.fileExists(atPath: filePath.path) else { return }
+        
+        do {
+            let data = try Data(contentsOf: filePath)
+            let user = try JSONDecoder().decode(UserInfo.self, from: data)
+            let wrapper = UserInfoWrapper(userInfo: user)
+            cache.setObject(wrapper, forKey: "authenticatedUser")
+        } catch {
+            fatalError("Failed to load user from disk: \(error)")
+        }
+    }
+}
---
+++ Eltex_Project/Eltex_Project/AppCoordinator/UserCacheService/UserInfoWrapper.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfoWrapper.swift
+//  Eltex_Project
+//
+//  Created by Vyacheslav Gusev on 23.12.2024.
+//
+
+import Foundation
+
+final class UserInfoWrapper: NSObject {
+    let userInfo: UserInfo
+    
+    init(userInfo: UserInfo) {
+        self.userInfo = userInfo
+    }
+}
