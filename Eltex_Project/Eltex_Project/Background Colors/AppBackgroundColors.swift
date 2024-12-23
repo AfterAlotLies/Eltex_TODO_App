fdfcb84fb05c812ec23e1793e60350a722304f91
Vyacheslav <guzxaroh@gmail.com>
2024-12-24 02:26:20 +0700

    added Constants colors

---
+++ Eltex_Project/Eltex_Project/Background Colors/AppBackgroundColors.swift
@@ -0,0 +1,13 @@
+//
+//  AppBackgroundColors.swift
+//  Eltex_Project
+//
+//  Created by Vyacheslav Gusev on 23.12.2024.
+//
+
+import UIKit
+
+enum AppBackgroundColors {
+    static let topColor: UIColor = UIColor(red: 18.0 / 255.0, green: 83.0 / 255.0, blue: 170.0 / 255.0, alpha: 1)
+    static let bottomColor: UIColor = UIColor(red: 5.0 / 255.0, green: 36.0 / 255.0, blue: 62.0 / 255.0, alpha: 1)
+}
